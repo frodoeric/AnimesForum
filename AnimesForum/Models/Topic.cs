134ef1a3e69bce2a9a6d7a3abd039433672b9a60
Eric Costa <eric.costa@hitachivantara.com>
2024-01-21 12:05:45 +0000

    added Topic model

---
+++ AnimesForum/Models/Topic.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimesForum.Models
+{
+    public class Topic
+    {
+        public int TopicId { get; set; }
+
+        [StringLength(100, ErrorMessage = "The title cannot be longer than 100 characters.")]
+        public required string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public required string Description { get; set; }
+
+        public required string Creator { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime CreationDate { get; set; }
+    }
+}
